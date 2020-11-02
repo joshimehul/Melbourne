acaddba1725ffbffe197f39359b2be3f50afa59a
jkugsiya <jkugsiya11@gmail.com>
2020-11-02 21:17:47 +0530

    deleting now adjusts its below fields to one upper

--- Melbourne-2/index.js
+++ Melbourne-2/index.js
@@ -4,4 +4,14 @@ function ClearContent(id)
     var commentId = "comment" + id;
     document.getElementById(tagId).value = "";
     document.getElementById(commentId).value = "";
+    if(id!=5)
+    {
+        for(var i=id; i<5; i++)
+        {
+            document.getElementById("tag" + parseInt(i)).value = document.getElementById("tag"+ (parseInt(i)+1)).value;
+            document.getElementById("comment" + parseInt(i)).value = document.getElementById("comment"+ (parseInt(i)+1)).value;
+            document.getElementById("tag" + (parseInt(i)+1)).value = "";
+            document.getElementById("comment" + (parseInt(i)+1)).value = "";
+        }
+    }
 }
